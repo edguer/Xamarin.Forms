ed78b7ac796c35d8f9b86673b1a313658c184a8c
E.Z. Hart <hartez@gmail.com>
2018-04-02 15:29:33 -0600

    Repro of multi-window issue (missing RootContainerStyle)

--- EmbeddingTestBeds/Embedding.UWP/MainPage.xaml.cs
+++ EmbeddingTestBeds/Embedding.UWP/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Embedding.XF;
 using Xamarin.Forms.Platform.UWP;
@@ -36,6 +40,23 @@ namespace Embedding.UWP
 		void NavToAlertsAndActionSheets(object sender, RoutedEventArgs e)
 		{
 			Frame.Navigate(new AlertsAndActionSheets());
+		}
+
+		async void ShowSecondWindow(object sender, RoutedEventArgs e)
+		{
+			CoreApplicationView newView = CoreApplication.CreateNewView();
+			int newViewId = 0;
+			await newView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+			{
+				Frame frame = new Frame();
+				frame.Navigate(typeof(SecondPage), null);   
+				Window.Current.Content = frame;
+				// You have to activate the window in order to show it later.
+				Window.Current.Activate();
+
+				newViewId = ApplicationView.GetForCurrentView().Id;
+			});
+			bool viewShown = await ApplicationViewSwitcher.TryShowAsStandaloneAsync(newViewId);
 		}
 	}
 }
